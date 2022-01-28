e8ade8ccaa9c66ca54208f248e056b351497facb
izaquielnn <izaquiel.nascimento@ccc.ufcg.edu.br>
2022-01-28 17:15:46 -0300

    aponta url para o ambiente no heroku

--- lib/app/config.dart
+++ lib/app/config.dart
@@ -1,4 +1,3 @@
 class Config {
-  static const String apiURL =
-      "http://192.168.1.100:3000"; //"https://cidade-singular-api-dev.herokuapp.com";
+  static const String apiURL = "https://cidade-singular-api-dev.herokuapp.com";
 }
